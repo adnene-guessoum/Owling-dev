7816c80a8d03c6ca4a7cd0d4a17bde7a7afce5e7
Adnene_wsl <adnen.guessoum@gmail.com>
2022-11-16 20:40:07 +0100

    Add some content on Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
+
 const Page = () => {
-	return <div>Hello World!</div>
+	return (
+		<Container>
+			<Box borderRadius='md' bg='red' p={4} mb={6} align="center">
+				Bienvenue sur Owling dev! Je suis Adnene,
+				développeur Python et javascript.
+			</Box>
+
+			<Box display={{md:'flex'}}>
+				<Box flexGrow={1}>
+					<Heading as="h2" variant="page-title">
+						Adnene Guessoum
+					</Heading>
+		<p>Owling dev (Développeur / Data Scientist / Lecteur passionée
+			et joueur assidue)</p>
+				</Box>
+			</Box>
+		</Container>
+	) 
 }
 
 export default Page
