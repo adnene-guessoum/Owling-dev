c6a5d798039e334d3dcf9e4a6f85bb7a1c99d864
Adnene_wsl <adnen.guessoum@gmail.com>
2022-11-17 14:46:27 +0100

    Add a navbar with some logo

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,6 +1,9 @@
 // Composant 'built-in' next.js pour ajouter (append) éléments à 'Head'
 import Head from 'next/head'
 
+// importer notre barre de navigation
+import Navbar from '../navbar.js'
+
 // Box pour page d'accueil et Container pour cotrôler largeur d'un contenu
 // cf composants chakra-ui
 import { Box, Container } from '@chakra-ui/react'
@@ -13,12 +16,14 @@ const Main = ({children, router}) => {
 				// viewport basique tenant compte des écrans smartphone
 				<meta name="viewport" content="width=device-width, initial-scale=1"/>
 				<title>Adnene Guessoum - Accueil</title>
+			</Head>
+
+			<Navbar path={router.asPath}/>
 
 				// width max taille medium (props du composant) + padding-top
 				<Container maxW="container.md" pt={14}>
 					{children}
 				</Container>
-			</Head>
 		</Box>
 	)
 }
--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,27 @@ import { Container, Box, Heading } from '@chakra-ui/react'
 const Page = () => {
 	return (
 		<Container>
-			<Box borderRadius='md' bg='red' p={4} mb={6} align="center">
-				Bienvenue sur Owling dev! Je suis Adnene,
-				développeur Python et javascript.
-			</Box>
 
-			<Box display={{md:'flex'}}>
-				<Box flexGrow={1}>
-					<Heading as="h2" variant="page-title">
-						Adnene Guessoum
-					</Heading>
+		<Box borderRadius='md' bg='red' p={4} mb={6} align="center">
+		Bienvenue sur Owling dev! Je suis Adnene,
+		développeur Python et javascript.
+		</Box>
+
+		<Box display={{md:'flex'}}>
+
+		<Box flexGrow={1}>
+
+		<Heading as="h2" variant="page-title">
+		Adnene Guessoum
+		</Heading>
+
 		<p>Owling dev (Développeur / Data Scientist / Lecteur passionée
 			et joueur assidue)</p>
-				</Box>
-			</Box>
+
+		</Box>
+
+		</Box>
+
 		</Container>
 	) 
 }
