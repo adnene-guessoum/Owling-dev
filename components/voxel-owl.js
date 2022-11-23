6d9f3a27b48148dfcf37823572a51cad6dc5ec1a
Adnene_wsl <adnen.guessoum@gmail.com>
2022-11-23 14:27:02 +0100

    Add Publications page
    Publications pour posts sur blog django en construction
    ou tous types de liens exterieurs vers présentation des différents
    projets.

--- components/voxel-owl.js
+++ components/voxel-owl.js
@@ -9,87 +9,70 @@ function easeOutCirc(x) {
   return Math.sqrt(1 - Math.pow(x - 1, 4));
 }
 
+// other attempt
 const VoxelOwl = () => {
-  const refContainer = useRef();
-  const [loading, setLoading] = useState(true);
-	const refRenderer = useRef()
-  const [renderer, setRenderer] = useState();
-  const [_camera, setCamera] = useState();
-	{/*
-  const [target] = useState(new THREE.Vector3(-0.5, 1.2, 0));
-  const [initialCameraPosition] = useState(
-    new THREE.Vector3(
-      20 * Math.sin(0.2 * Math.PI),
-      10,
-      20 * Math.cos(0.2 * Math.PI)
-    )
-  );
-
-  const [scene] = useState(new THREE.Scene());
-	*/}
-  const [_controls, setControls] = useState();
+  const refContainer = useRef()
+  const [loading, setLoading] = useState(true)
+  const refRenderer = useRef()
 
   const handleWindowResize = useCallback(() => {
-		const { current: renderer } = refRenderer
-    const { current: container } = refContainer;
+    const { current: renderer } = refRenderer
+    const { current: container } = refContainer
     if (container && renderer) {
-      const scW = container.clientWidth;
-      const scH = container.clientHeight;
+      const scW = container.clientWidth
+      const scH = container.clientHeight
 
-      renderer.setSize(scW, scH);
+      renderer.setSize(scW, scH)
     }
-  }, []);
+  }, [])
 
   /* eslint-disable react-hooks/exhaustive-deps */
   useEffect(() => {
-    const { current: container } = refContainer;
-    if (container && !renderer) {
-      const scW = container.clientWidth;
-      const scH = container.clientHeight;
+    const { current: container } = refContainer
+    if (container) {
+      const scW = container.clientWidth
+      const scH = container.clientHeight
 
       const renderer = new THREE.WebGLRenderer({
         antialias: true,
         alpha: true
-      });
-      renderer.setPixelRatio(window.devicePixelRatio);
-      renderer.setSize(scW, scH);
-      renderer.outputEncoding = THREE.sRGBEncoding;
-      container.appendChild(renderer.domElement);
-			{/*setRenderer(renderer);*/}
-			refRenderer.current = renderer
-			const scene = new THREE.Scene()
+      })
+      renderer.setPixelRatio(window.devicePixelRatio)
+      renderer.setSize(scW, scH)
+      renderer.outputEncoding = THREE.sRGBEncoding
+      container.appendChild(renderer.domElement)
+      refRenderer.current = renderer
+      const scene = new THREE.Scene()
 
-			const target = new THREE.Vector3(-0.5, 1.2, 0)
+      const target = new THREE.Vector3(-0.5, 1.2, 0)
       const initialCameraPosition = new THREE.Vector3(
-        20 * Math.sin(0.2 * Math.PI),
-        10,
-        20 * Math.cos(0.2 * Math.PI)
+        20 * Math.sin(0.7 * Math.PI),
+        50,
+        20 * Math.cos(0.7 * Math.PI)
       )
 
       // 640 -> 240
-      // 8 -> 6
-      const scale = scH * 0.005 + 4.8;
+      // 8   -> 6
+      const scale = scH * 0.0005 + 4.8
       const camera = new THREE.OrthographicCamera(
         -scale,
         scale,
         scale,
         -scale,
-        0.01,
-        50000
-      );
-      camera.position.copy(initialCameraPosition);
-      camera.lookAt(target);
-      setCamera(camera);
+        0.0001,
+        500
+      )
+      camera.position.copy(initialCameraPosition)
+      camera.lookAt(target)
 
-      const ambientLight = new THREE.AmbientLight(0xcccccc, 1);
-      scene.add(ambientLight);
+      const ambientLight = new THREE.AmbientLight(0xcccccc, 1)
+      scene.add(ambientLight)
 
-      const controls = new OrbitControls(camera, renderer.domElement);
-      controls.autoRotate = true;
-      controls.target = target;
-      setControls(controls);
-
-      loadGLTFModel(scene, '/owl-baked.glb', {
+      const controls = new OrbitControls(camera, renderer.domElement)
+      controls.autoRotate = true
+      controls.target = target
+			
+			loadGLTFModel(scene, '/owl-baked.glb', {
         receiveShadow: false,
         castShadow: false
       }).then(() => {
@@ -97,57 +80,57 @@ const VoxelOwl = () => {
         setLoading(false);
       });
 
-      let req = null;
-      let frame = 0;
+      let req = null
+      let frame = 0
       const animate = () => {
-        req = requestAnimationFrame(animate);
+        req = requestAnimationFrame(animate)
 
-        frame = frame <= 100 ? frame + 1 : frame;
+        frame = frame <= 100 ? frame + 1 : frame
 
         if (frame <= 100) {
-          const p = initialCameraPosition;
-          const rotSpeed = -easeOutCirc(frame / 120) * Math.PI * 20;
+          const p = initialCameraPosition
+          const rotSpeed = -easeOutCirc(frame / 120) * Math.PI * 20
 
-          camera.position.y = 10;
+          camera.position.y = 10
           camera.position.x =
-            p.x * Math.cos(rotSpeed) + p.z * Math.sin(rotSpeed);
+            p.x * Math.cos(rotSpeed) + p.z * Math.sin(rotSpeed)
           camera.position.z =
-            p.z * Math.cos(rotSpeed) - p.x * Math.sin(rotSpeed);
-          camera.lookAt(target);
+            p.z * Math.cos(rotSpeed) - p.x * Math.sin(rotSpeed)
+          camera.lookAt(target)
         } else {
-          controls.update();
+          controls.update()
         }
 
-        renderer.render(scene, camera);
-      };
+        renderer.render(scene, camera)
+      }
 
       return () => {
-        cancelAnimationFrame(req);
-        renderer.dispose();
-      };
+        cancelAnimationFrame(req)
+        renderer.domElement.remove()
+        renderer.dispose()
+      }
     }
-  }, []);
+  }, [])
 
   useEffect(() => {
-    window.addEventListener('resize', handleWindowResize, false);
+    window.addEventListener('resize', handleWindowResize, false)
     return () => {
-      window.removeEventListener('resize', handleWindowResize, false);
-    };
-  }, [handleWindowResize]);
+      window.removeEventListener('resize', handleWindowResize, false)
+    }
+  }, [handleWindowResize])
+
 
   return (
 
     <Box
       ref={refContainer}
       className="voxel-owl"
-      m="auto"
-      mt={['-20px', '-60px', '-120px']}
-      mb={['-40px', '-140px', '-200px']}
+			m="auto"
+      mb={['-10px', '-100px', '-200px']}
       w={[280, 480, 640]}
       h={[280, 480, 640]}
       position="relative"
     >
-		{/*
       {loading && (
         <Spinner
           size="xl"
@@ -158,7 +141,6 @@ const VoxelOwl = () => {
           mt="calc(0px - var(--spinner-size))"
         />
       )}
-			*/}
     </Box>
   );
 };
